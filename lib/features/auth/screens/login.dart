6caa6650d4f8da4e72877f64ca2a91d04321dd57
Mahmut Can Yazlak <canmahmutyazlak@gmail.com>
2024-12-04 17:22:16 +0300

    Refactor login.dart to simplify imports and remove unused LoginMethods widget

--- lib/features/auth/screens/login.dart
+++ lib/features/auth/screens/login.dart
@@ -4,7 +4,7 @@ import 'package:snap_cart/config/extension/context_extension.dart';
 import '../../../config/items/app_colors.dart';
 import '../../../config/utility/constants/string_constants.dart';
 import '../../../config/utility/enum/image_constants.dart';
-import '../../../config/widget/sign_up/sign_up.dart';
+import '../widget/sign_up.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -124,52 +124,6 @@ class _LoginState extends State<Login> with SingleTickerProviderStateMixin {
                 ),
               )
             ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class LoginMethods extends StatelessWidget {
-  const LoginMethods({
-    super.key,
-    required this.labelText,
-    required this.image,
-  });
-
-  final String labelText;
-  final ImageConstants image;
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: InkWell(
-        onTap: () {},
-        child: Container(
-          decoration: BoxDecoration(
-            color: AppColors.lightGrayColor,
-            borderRadius: BorderRadius.circular(30),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Image.asset(
-                  image.getPng,
-                  width: context.width * 0.05,
-                  height: context.height * 0.05,
-                ),
-                Text(
-                  labelText,
-                  style: Theme.of(context)
-                      .textTheme
-                      .titleMedium
-                      ?.copyWith(color: AppColors.darkGrayColor),
-                ),
-              ],
-            ),
           ),
         ),
       ),
